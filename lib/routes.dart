ab31c7509334e35501e3d0863093ceab9c6f0dce
cmtan-2000 <cmtan0108@gmail.com>
2023-04-14 16:38:54 +0800

    initial commit

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:wmsm_component/view/widgetPage.dart';
+
+Map<String, WidgetBuilder> routes = {
+  '/': (context) => WidgetPage(),
+  // '/profile': (context) => ProfileScreen(),
+};
---
+++ lib/view/user_auth/signup_form.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../viewmodel/user_auth/authentication_model.dart';
+import '../custom/widgets/custom_button.dart';
+
+class WidgetSignUp extends StatefulWidget {
+  const WidgetSignUp({super.key});
+
+  @override
+  State<WidgetSignUp> createState() => _WidgetSignUpState();
+}
+
+class _WidgetSignUpState extends State<WidgetSignUp> {
+  final AuthenticationViewModel auth = AuthenticationViewModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account? "),
+        CustomTextButton(
+          text: 'Sign up',
+          context: context,
+          onPressed: () {},
+          textStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
